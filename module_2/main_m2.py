d280821f8bdb01a16aa55e35393f6e850401d2a6
mlyddon <lyddon.morg@gmail.com>
2025-03-22 09:40:29 -0500

    module 2 assignment

--- module_2/main_m2.py
+++ module_2/main_m2.py
@@ -1,5 +1,8 @@
 # Constants
 COST_PER_FOOT = 0.87
+COST_PER_FOOT_GREATER_100 = 0.80
+COST_PER_FOOT_GREATER_250 = 0.70
+COST_PER_FOOT_GREATER_500 = 0.50
 COMPANY_NAME = "Fast & Fibrous"
 
 def main():
@@ -8,13 +11,16 @@ def main():
 
     length_of_cable = get_cable_length()
 
-    cost_of_cable = calculate_cost_of_cable(length_of_cable)
+    # Get the cost of the cable per foot depending on the length of cable
+    cost_of_cable_per_foot = calculate_bulk_discount_price(length_of_cable)
+
+    cost_of_cable = calculate_cost_of_cable(length_of_cable, cost_of_cable_per_foot)
 
     #Print the cost of the cable. formate the cost to 2 decimal places
     print(f"The cost for {COMPANY_NAME} to install cable for {length_of_cable:,.2f} feet is ${cost_of_cable:,.2f}. Your invoice is below:")
     print()
 
-    print_invoice(COMPANY_NAME, length_of_cable, COST_PER_FOOT, cost_of_cable)
+    print_invoice(COMPANY_NAME, length_of_cable, cost_of_cable_per_foot, cost_of_cable)
 
 # Get the length of cable from the user
 def get_cable_length():
@@ -33,11 +39,25 @@ def get_cable_length():
             print()
 
 # Calculate the cost of the cable
-def calculate_cost_of_cable(cable_length: float) -> float:
+def calculate_cost_of_cable(cable_length: float, cost_of_cable_per_foot: float) -> float:
+    cost_of_cable = 0
+
     if cable_length <= 0:
         raise ValueError("Cable length must be a positive number")
 
-    return cable_length * COST_PER_FOOT
+    return round (cable_length * cost_of_cable_per_foot, 2)
+
+# Returns the cost of the cable per foot depending on the length of cable. This helps keep the cost calculation DRY and clean
+def calculate_bulk_discount_price(cable_length: float) -> float:
+    if cable_length > 500:
+        return COST_PER_FOOT_GREATER_500
+    elif cable_length > 250:
+        return COST_PER_FOOT_GREATER_250
+    elif cable_length > 100:
+        return COST_PER_FOOT_GREATER_100
+    else:
+        return COST_PER_FOOT
+
 
 # Print an ASCII invoice
 def print_invoice(company_name: str, length_of_cable: float, cost_per_foot: float, total_cost: float) -> None:
--- module_2/main_m2_test.py
+++ module_2/main_m2_test.py
@@ -1,19 +1,45 @@
 import unittest
-from main_m2 import calculate_cost_of_cable, COST_PER_FOOT
+from main_m2 import calculate_cost_of_cable
+from module_2.main_m2 import calculate_bulk_discount_price
+
+#Constants
+COST_PER_FOOT = 0.87
+COST_PER_FOOT_GREATER_100 = 0.80
+COST_PER_FOOT_GREATER_250 = 0.70
+COST_PER_FOOT_GREATER_500 = 0.50
+
 
 class TestCableCalculator(unittest.TestCase):
     def test_given_valid_length_of_cable_then_return_cost_of_cable(self):
-        self.assertEqual(calculate_cost_of_cable(10), 8.7)
-        self.assertEqual(calculate_cost_of_cable(100), 87.0)
-        self.assertEqual(calculate_cost_of_cable(1.5), 1.305)
+        self.assertEqual(calculate_cost_of_cable(10, COST_PER_FOOT), 8.7)
+        self.assertEqual(calculate_cost_of_cable(100, COST_PER_FOOT), 87.0)
+        self.assertEqual(calculate_cost_of_cable(1.5, COST_PER_FOOT), 1.30)
 
     def test_given_length_of_zero_then_raise_value_error(self):
         with self.assertRaises(ValueError):
-            calculate_cost_of_cable(0)
+            calculate_cost_of_cable(0, COST_PER_FOOT)
 
     def test_given_negative_length_then_raise_value_error(self):
         with self.assertRaises(ValueError):
-            calculate_cost_of_cable(-10)
+            calculate_cost_of_cable(-10, COST_PER_FOOT)
+
+    def test_given_501_feet_of_cable_then_return_cost_of_cable(self):
+        self.assertEqual(calculate_cost_of_cable(501, COST_PER_FOOT_GREATER_500), 250.5)
+
+    def test_given_251_feet_of_cable_then_return_cost_of_cable(self):
+        self.assertEqual(calculate_cost_of_cable(251, COST_PER_FOOT_GREATER_250), 175.7)
+
+    def test_given_101_feet_of_cable_then_return_cost_of_cable(self):
+        self.assertEqual(calculate_cost_of_cable(101, COST_PER_FOOT_GREATER_100), 80.8)
+
+    def test_calculate_bulk_discount_price(self):
+        self.assertEqual(calculate_bulk_discount_price(50), COST_PER_FOOT)
+        self.assertEqual(calculate_bulk_discount_price(100), COST_PER_FOOT)
+        self.assertEqual(calculate_bulk_discount_price(101), COST_PER_FOOT_GREATER_100)
+        self.assertEqual(calculate_bulk_discount_price(250), COST_PER_FOOT_GREATER_100)
+        self.assertEqual(calculate_bulk_discount_price(251), COST_PER_FOOT_GREATER_250)
+        self.assertEqual(calculate_bulk_discount_price(500), COST_PER_FOOT_GREATER_250)
+        self.assertEqual(calculate_bulk_discount_price(501), COST_PER_FOOT_GREATER_500)
 
 if __name__ == '__main__':
     unittest.main()
